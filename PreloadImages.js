902361e15dc24efb46ee87e6c67ac1ae1d2a3639
Blupguy <joedunken279@gmail.com>
2024-02-03 20:43:46 -0900

    2024.2
    -Trying to add preloading for images
    - Changed Title in index.html

---
+++ PreloadImages.js
@@ -0,0 +1,30 @@
+var images = [
+    "Images/Cow.png",
+    "Images/tpose.png",
+    "Images/yeehawfrog.png",
+    "Images/blupFrog.png",
+    "Images/froog].png",
+    "Images/Miata.png",
+    "Images/OldPNGTuber.png",
+    "Images/sadfrog.png",
+    "Images/sunset.png",
+    "Images/TLsDuck.png",
+    "Images/frog.png",
+    "Images/prettysunset.jpg",
+    "Images/sun.jpg",
+    "Images/mountain.jpg",
+    "Images/Yoru.jpg",
+    "Images/PrettySky.JPG",
+    "Images/cookiedough.JPG",
+    "Images/goofycar.png",
+    "Images/YoruStare.png",
+    "Images/goofyahhcat.JPG",
+    "Images/NightSky.JPG",
+    "Images/TestSquatchy.gif"
+
+  ];
+
+  image.forEach(function(imageSrc) => {
+      var img = new Images();
+      img.src = imageSrc;
+  });
